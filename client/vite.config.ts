f64d9e0da31290c4996c03ac0d62b170a9a6e590
rsystems-8601 <pradeep.srivastav@rsystems.com>
2023-12-03 21:23:35 +0530

    update vite

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/demo-app",
+  base: "https://demo-app-six-lilac.vercel.app/",
   resolve: {
     alias: {
       "@": "/src",
